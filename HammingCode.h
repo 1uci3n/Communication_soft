8c16693c605e2719e44af54f7f4949272b27c9f1
1uci3n <giranten@outlook.com>
2018-02-03 23:39:38 +0900

    fix up the HammingCode.h

--- HammingCode.h
+++ HammingCode.h
@@ -1,9 +1,12 @@
 #include<stdio.h>
 #include<math.h>
 
+
 char* encode(char message[], int msgLength){
+	//计算块的大小
 	int blockLength = getBlockLength(msgLength);
-	int 
+	//计算纠错位的长度
+	int checkLength = blockLength - msgLength;
 	char block[blockLength];
 	printf("%d\n", blockLength);
 	return block;
