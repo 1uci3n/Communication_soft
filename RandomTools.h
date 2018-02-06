2da176427cb25398d45023964edb7384343d5874
1uci3n <giranten@outlook.com>
2018-02-06 23:57:37 +0900

    a little fix up

--- RandomTools.h
+++ RandomTools.h
@@ -3,11 +3,11 @@
 #include<time.h>
 #include<math.h>
 int* getRandomIntArray(int length, int unitLength){
-    int array[lenght];
-    srand(time(NULL));
-    int randomTool = pow(10, unitLength);
-    for(int i = 0; i < length; i++){
-        array[i] = rand() % (pow(10, randomTool);
-    }
-    return array;
+	int array[lenght];
+	srand(time(NULL));
+	int randomTool = pow(10, unitLength);
+	for(int i = 0; i < length; i++){
+		array[i] = rand() % (pow(10, randomTool);
+	}
+	return array;
 }
