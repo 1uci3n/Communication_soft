a842848ee7791dccd05fe8131132c9fd86d8f537
1uci3n <giranten@outlook.com>
2018-02-15 23:48:30 +0800

    add some comments and -------------Happy New Year

--- HammingCode.h
+++ HammingCode.h
@@ -4,6 +4,7 @@
 
 #define chartonumber(x) (x-'0')
 #define MAX 1000000
+#define TEST 1
 
 char* encode(char message[], int msgLength);
 int getBlockLength(int msgLength);
@@ -13,28 +14,29 @@ char* encode(char message[], int msgLength){
 	//计算块的大小
 	int blockLength = getBlockLength(msgLength);
 	//块大小的调试信息
-	printf("%d\n", blockLength);
+	if (TEST == 1){
+		printf("%d\n", blockLength);
+	}
 	//计算纠错位的长度
 	int checkbitLength = blockLength - msgLength;
 	//初始化纠错位的位置
 	int checkbitPosition[checkbitLength];
 	getCheckbitPosition(checkbitPosition, checkbitLength);
 	//纠错位位置的调试信息
-	for (int i = 0; i < checkbitLength; i++){
-		printf("%d ", checkbitPosition[i]);
+	if (TEST == 1){
+		for (int i = 0; i < checkbitLength; i++){
+			printf("%d ", checkbitPosition[i]);
+		}
+		printf("\n");
 	}
-	printf("\n");
 	//产生临时编码块
 	char tempBlock[blockLength];
 	//当前的纠错位序号
 	int checkbitIndex = 0;
 	//当前的数据位序号
 	int msgbitIndex = 0;
-	//储存临时的纠错信息
-
 	//对纠错位进行搜索
 	for (int i = 0; i < blockLength; ++i){
-		
 		//判断当前位是否为纠错位
 		if (i == checkbitPosition[checkbitIndex]){
 			// //是纠错位,计算纠错位的数据
@@ -58,38 +60,62 @@ char* encode(char message[], int msgLength){
 	int tempData = 0;
 	//纠错位序号初始化
 	checkbitIndex = 0;
+	//纠错位内层循环子
 	int j;
 	//纠错位的计算
-	printf("开始纠错位计算\n");
+	//纠错位计算开始的调试信息
+	if (TEST == 1){
+		printf("开始纠错位计算\n");
+	}
 	for (int i = 0; i < blockLength; ++i)
 	{
 		//判断当前位是否为纠错位
 		if (tempBlock[i] == 'X')
 		{
 			//如果为纠错位，计算纠错位数据，配偶原则
+			//内层循环因子初始化，计算纠错位数据，从当前的纠错位开始，与之前的位无关
 			j = i;
+			//连续标记，用来标记同一组内连续的数据位的序数
 			nowNumber = 0;
-			printf("checkbitPosition[checkbitIndex] %d\n", checkbitPosition[checkbitIndex]);
+			//纠错位计算分组的调试信息
+			if (TEST == 1){
+				printf("checkbitPosition[checkbitIndex] %d\n", checkbitPosition[checkbitIndex]);
+			}
+			//当内层循环因子小于块的长度时，循环继续
 			while(j < blockLength){
-				printf("s %c 第%d位\n", tempBlock[j],j + 1);
+				//纠错位计算的调试信息
+				if (TEST == 1){
+					printf("s %c 第%d位\n", tempBlock[j],j + 1);
+				}
+				//为了跳过分组中的第一位，对当前操作位进行判断是否位该组的第一位
 				if (j != i){
+					//若当前操作位不为当前组的第一位，进行累加计算
 					tempData = tempData + chartonumber(tempBlock[j]);
 				}
+				//连续标记自加
 				nowNumber++;
+				//对连续标记与当前纠错位的位置加一进行比较
 				if (nowNumber < checkbitPosition[checkbitIndex] + 1){
+					//当连续标记小于纠错位的位置加一，j自加，即j的下一位也在当前的分组中
 					j++;
 				} else {
+					//当连续标记大于等于纠错位的位置加一，j“指针”需要跳到下一段连续数据
 					j= j + checkbitPosition[checkbitIndex] + 2;
+					//将连续标记重置为零
 					nowNumber = 0;
 				}
 			}
-			printf("%d\n",tempData);
+			if (TEST == 1){
+				printf("%d\n",tempData);
+			}
 			if ((tempData % 2) == 0){
 				tempBlock[i] = '0';
 			} else {
 				tempBlock[i] = '1';
 			}
+			//每次循环完成后，对一些每次循环需要用到的数据进行初始化
 			tempData = 0;
+			//
 			nowNumber = 0;
 			//纠错位序号加一
 			checkbitIndex++;
