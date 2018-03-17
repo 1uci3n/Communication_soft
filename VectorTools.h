332022962d314253c7c3d15c73e40beec05dad68
1uci3n <giranten@outlook.com>
2018-03-18 01:40:36 +0900

    help to print vector

---
+++ VectorTools.h
@@ -0,0 +1,26 @@
+#include<vector>
+#include<string>
+
+void printVector(std::vector<int> v){
+	for (std::vector<int>::iterator i = v.begin(); i != v.end(); ++i)
+	{
+		std::cout << *i << " ";
+	}
+	std::cout << std::endl;
+}
+
+void printVector(std::vector<char> v){
+	for (std::vector<char>::iterator i = v.begin(); i != v.end(); ++i)
+	{
+		std::cout << *i << " ";
+	}
+	std::cout << std::endl;
+}
+
+void printVector(std::vector<double> v){
+	for (std::vector<double>::iterator i = v.begin(); i != v.end(); ++i)
+	{
+		std::cout << *i << " ";
+	}
+	std::cout << std::endl;
+}
