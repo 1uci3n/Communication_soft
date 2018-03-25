4d06fcd57c8f026271508c67c8c8661f09f152a6
1uci3n <giranten@outlook.com>
2018-03-25 18:59:20 +0900

    add a new way to simulate Gaussian channel

--- OutputEffective.cpp
+++ OutputEffective.cpp
@@ -2,11 +2,12 @@
 #include <vector>
 #include "HammingCodeCpp.h"
 #include "RandomTools.h"
-#include "GaussianChannel.h"
+//#include "GaussianChannel.h"
+#include "GaussianChannel2nd.h"
 #include "OutputTools.h"
 
 //信噪比最小值
-#define MINI_S_N_R 0
+#define MINI_S_N_R 0.1
 //信噪比最大值
 #define MAX_S_N_R 10
 //测试信噪比的间隔
@@ -16,14 +17,23 @@
 // //信噪比
 // #define S_N_R 6;
 
+/**
+ * [compareBolckBit description]
+ * @param  block1 [description]
+ * @param  block2 [description]
+ * @return        [description]
+ */
+int compareBolckBit(std::vector<int> block1, std::vector<int> block2);
+
 int main(int argc, char const *argv[])
 {
 	//最终输出的数据map
 	std::map<double, std::vector<double> > resultMap;
 	//数据map的keySet
 	std::vector<double> keySet;
-	//用来储存每一轮结果的容器,[0]存储硬解码失败率，[1]存储最大似然度解码失败率
-	std::vector<double> resultV(2);
+	//用来储存每一轮结果的容器,[0]存储硬解码位错误率,[1]存储硬解码词错误率
+	//[2]存储最大似然度位错误率,[3]存储最大似然度词错误率
+	std::vector<double> resultV(4);
 	//消息储存器
 	std::vector<int> msg;
 	//编码后的块储存器
@@ -34,10 +44,14 @@ int main(int argc, char const *argv[])
 	std::vector<int> tempHR;
 	//内层循环每次最大似然解码的结果临时存储器
 	std::vector<int> tempMR;
-	//硬解码失败计数器
-	int hFailCount;
-	//最大似然度解码失败计数器
-	int mFailCount;
+	//硬解码错误词计数器
+	int hWErrorCount;
+	//最大似然度解码错误词计数器
+	int mWErrorCount;
+	//硬解码错误位计数器
+	int hBErrorCount;
+	//最大似然度解码错误位计数器
+	int mBErrorCount;
 	//设置随机函数需要用到的盐值
 	srand((unsigned)time(NULL));
 	//设置最大似然解码方式需要用到的编码表
@@ -45,9 +59,11 @@ int main(int argc, char const *argv[])
 	//不同信道测试的循环
 	for (double snr = MINI_S_N_R; snr <= MAX_S_N_R; snr += S_N_R_INTERVAL)
 	{
-		//初始化失败计数器
-		hFailCount = 0;
-		mFailCount = 0;
+		//初始化错误计数器
+		hWErrorCount = 0;
+		mWErrorCount = 0;
+		hBErrorCount = 0;
+		mBErrorCount = 0;
 		//每个信道测试的循环体
 		for (int i = 0; i < COUNT; ++i)
 		{
@@ -56,22 +72,29 @@ int main(int argc, char const *argv[])
 			//将消息编码
 			block = encode(msg, msg.size());
 			//将块放入模拟的高斯信道传输
-			clientBlock = Gaussian_channel(block, getN0ByDbSNR(snr));
+			//clientBlock = Gaussian_channel(block, getN0ByDbSNR(snr));
+			clientBlock = BPSK_AWGNC(block, getVarianceBySNRdB(snr));
 			//硬解码
 			tempHR = decodeHard(clientBlock, 7);
 			//最大似然解码
 			tempMR = decodeMLD(clientBlock, 4);
 			//对解码结果进行判断
 			if(tempHR != msg){
-				hFailCount++;
+				hWErrorCount++;
+				hBErrorCount += compareBolckBit(msg, tempHR);
 			}
 			if(tempMR != msg){
-				mFailCount++;
+				mWErrorCount++;
+				mBErrorCount += compareBolckBit(msg, tempMR);
 			}
 		}
-		//计算失败率并记入容器
-		resultV[0] = hFailCount / (double) COUNT;
-		resultV[1] = mFailCount / (double) COUNT;
+		//计算块失败率并记入容器
+		//[0]存储硬解码位错误率,[1]存储硬解码词错误率
+		//[2]存储最大似然度位错误率,[3]存储最大似然度词错误率
+		resultV[0] = hBErrorCount / ((double) COUNT * 4);
+		resultV[1] = hWErrorCount / (double) COUNT;
+		resultV[2] = mBErrorCount / ((double) COUNT * 4);
+		resultV[3] = mWErrorCount / (double) COUNT;
 		//将结果的容器存入Map中
 		resultMap.insert(std::pair<double, std::vector<double> >(snr, resultV));
 		//将Key存入keySet中
@@ -81,4 +104,16 @@ int main(int argc, char const *argv[])
 	char fileName[] = "data.txt";
 	outputMap(resultMap, keySet, fileName);
 	return 0;
+}
+
+int compareBolckBit(std::vector<int> block1, std::vector<int> block2){
+	int diffCount = 0;
+	for (int i = 0; i < block1.size(); ++i)
+	{
+		if (block1[i] != block2[i])
+		{
+			diffCount++;
+		}
+	}
+	return diffCount;
 }
